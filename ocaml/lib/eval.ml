6bf6acdd6537cbfb5fc08e4a7f9f6b046f2d3c34
Matt Teichman <teichman@uchicago.edu>
2023-04-21 10:57:47 -0500

    made eval stuff explicit

--- ocaml/lib/eval.ml
+++ ocaml/lib/eval.ml
@@ -13,7 +13,9 @@ let eval_value (type a) (v : a value) : a =
   | Int x -> x
   | Bool x -> x
 
-let rec eval : type a. a expr -> a = function
+let rec eval : 'a. 'a expr -> 'a =
+  fun (type a) (e : a expr) ->
+  match e with
   | Value v -> eval_value v
   | If (c, t, e) -> if eval c then eval t else eval e
   | Eq (x, y) -> eval x = eval y
